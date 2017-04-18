3a12ddf31ff69b04b5d93c54743f462d906fc069
kakucd <kakucd@plu.edu>
2017-04-17 17:33:29 -0700

    new commit

---
+++ app/src/main/java/com/httpstwitter/birdfeedadmin/MainActivity.java
@@ -0,0 +1,160 @@
+package com.httpstwitter.birdfeedadmin;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.JsonReader;
+import android.util.JsonToken;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Tweet> data = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                try {
+                    loadJSONFromAsset();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void push() {
+
+    }
+
+    public void loadJSONFromAsset() throws JSONException {
+        InputStream is = null;
+        try {
+
+            is = getAssets().open("restaurant.json");
+            //int size = is.available();
+            //byte[] buffer = new byte[size];
+            //is.read(buffer);
+            //is.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.println("json String initialized");
+        //JSONObject obj = new JSONObject(json);
+        JsonReader reader = null;
+        try {
+            reader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Exception caught");
+            e.printStackTrace();
+        }
+
+        try {
+            assert reader != null;
+            reader.beginObject();
+            reader.nextName();
+            reader.beginArray();
+            while (reader.hasNext() && reader.peek() != JsonToken.END_DOCUMENT) {
+                System.out.println(reader.peek());
+                data.add(readTweet(reader));
+            }
+            //reader.endObject();
+        } catch (IOException e) {
+            for(int i = 0; i < data.size(); i++) {
+                System.out.println("data: "+data.get(i));
+            }
+            e.printStackTrace();
+        }
+
+    }
+
+    public Tweet readTweet(JsonReader reader) throws IOException {
+        String text = null;
+        String user = null;
+
+        while (reader.hasNext()) {
+            String name;
+            try {
+                name = reader.nextString();
+                System.out.println("Name: "+name);
+            } catch(Exception e) {
+                System.out.println("Exception caught: "+e.toString());
+                break;
+            }
+            if (name.equals("user")) {
+                user = reader.nextString();
+                System.out.println("User: " + user);
+            } else if (name.equals("text")) {
+                text = reader.nextString();
+                System.out.println("Text: " + text);
+            } else {
+                reader.skipValue();
+                reader.endObject();
+            }
+        }
+        return new Tweet(user, text);
+    }
+
+    public class Tweet {
+        String user;
+        String tweet;
+
+        public Tweet() {
+            user = null;
+            tweet = null;
+        }
+
+        public Tweet(String u, String t) {
+            user = u;
+            tweet = t;
+        }
+    }
+}
